fee5b66c3f106822f3500b36fef49b6713000fa5
hieunm342003 <nguyenminhhieu342003@gmail.com>
2026-02-18 03:59:30 +0700

    deloy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,31 @@ import tasksRouter from './routes/tasksRouters.js';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 dotenv.config();
 const PORT = process.env.PORT || 5001;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const projectRoot = path.resolve(__dirname, '..', '..');
 const app = express();
-const corsOptions = {
-    origin: 'http://localhost:5173', // Thay đổi theo địa chỉ frontend của bạn
-    optionsSuccessStatus: 200,
-  };
-app.use(cors(corsOptions));
+if (process.env.NODE_ENV !== 'production') {
+
+    const corsOptions = {
+        origin: 'http://localhost:5173', // Thay đổi theo địa chỉ frontend của bạn
+        optionsSuccessStatus: 200,
+    };
+    app.use(cors(corsOptions));
+}
 app.use(express.json());
 app.use('/api/tasks', tasksRouter);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(projectRoot, 'frontend', 'dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(projectRoot, 'frontend', 'dist', 'index.html'));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
